5004b71e96a6aed4e8ab2ce9dd5c33834b033688
Brian Munro <brian.alexander.munro@gmail.com>
2016-11-26 13:35:09 +0200

    Updated plublish.bat file to work with latest changes.

--- publish.bat
+++ publish.bat
@@ -1,46 +1,59 @@
-set _NMMPATH=%~dp0%NMM
+set _NMMPATH=%~dp0..\nmm
+set _NMMCLIPATH=%~dp0
+set outputPath=..\..\..\install\bin
 
-mkdir ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\ChinhDo.Transactions.dll ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\Commanding.dll ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\GamebryoBase.dll ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\ICSharpCode.TextEditor.dll ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\ModManager.Interface.dll ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\Mods.dll ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\NexusClient.Interface.dll ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\NexusClientCLI.exe ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\NexusClientCLI.exe.config ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\NexusClientCLI.exe.manifest ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\Scripting.dll ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\SevenZipSharp.dll ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\Transactions.dll ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\UI.dll ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\Util.dll ..\..\output\NCC
-copy %_NMMPATH%\bin\Release\WeifenLuo.WinFormsUI.Docking.dll ..\..\output\NCC
+mkdir  "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\ChinhDo.Transactions.dll" "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\Commanding.dll" "%outputPath%\NCC"
+rem copy %_NMMPATH%\bin\Release\GamebryoBase.dll" "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\ICSharpCode.TextEditor.dll" "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\ModManager.Interface.dll" "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\Mods.dll" "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\NexusClient.Interface.dll" "%outputPath%\NCC"
+rem copy %_NMMPATH%\bin\Release\NexusClientCLI.exe.manifest" "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\ObjectListView.dll" "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\RestSharp.dll" "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\Scripting.dll" "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\SevenZipSharp.dll" "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\Transactions.dll" "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\UI.dll" "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\Util.dll" "%outputPath%\NCC"
+copy "%_NMMPATH%\bin\Release\WeifenLuo.WinFormsUI.Docking.dll" "%outputPath%\NCC"
+rem stored in repository in binary form
+copy "%_NMMPATH%\bin\Release\Castle.Core.dll" "%outputPath%\NCC"
 
-mkdir ..\..\output\NCC\GameModes
-copy %_NMMPATH%\bin\Release\GameModes\Fallout3.* ..\..\output\NCC\GameModes
-copy %_NMMPATH%\bin\Release\GameModes\FalloutNV.* ..\..\output\NCC\GameModes
-copy %_NMMPATH%\bin\Release\GameModes\Skyrim.* ..\..\output\NCC\GameModes
-copy %_NMMPATH%\bin\Release\GameModes\Oblivion.* ..\..\output\NCC\GameModes
-copy %_NMMPATH%\bin\Release\GameModes\GamebryoBase.dll ..\..\output\NCC\GameModes
+mkdir  "%outputPath%\NCC\GameModes"
+copy "%_NMMPATH%\bin\Release\GameModes\Fallout3.*" "%outputPath%\NCC\GameModes"
+copy "%_NMMPATH%\bin\Release\GameModes\Fallout4.*" "%outputPath%\NCC\GameModes"
+copy "%_NMMPATH%\bin\Release\GameModes\FalloutNV.*" "%outputPath%\NCC\GameModes"
+copy "%_NMMPATH%\bin\Release\GameModes\Skyrim.*" "%outputPath%\NCC\GameModes"
+copy "%_NMMPATH%\bin\Release\GameModes\SkyrimSE.*" "%outputPath%\NCC\GameModes"
+copy "%_NMMPATH%\bin\Release\GameModes\Oblivion.*" "%outputPath%\NCC\GameModes"
+copy "%_NMMPATH%\bin\Release\GameModes\GamebryoBase.dll" "%outputPath%\NCC\GameModes"
 
-mkdir ..\..\output\NCC\GameModes\data
-echo copy bin\Release\BossDummy.dll ..\..\output\NCC\data\boss32.dll
-echo copy bin\Release\BossDummy.dll ..\..\output\NCC\GameModes\data\boss32.dll
-echo copy bin\Release\BossDummy.dll ..\..\output\NCC\data\boss64.dll
-echo copy bin\Release\BossDummy.dll ..\..\output\NCC\GameModes\data\boss64.dll
+mkdir  "%outputPath%\NCC\GameModes\data"
+rem copy %_NMMPATH%\..\bin\Release\BossDummy.dll" "%outputPath%\NCC\GameModes\data\boss32.dll"
 
-mkdir ..\..\output\NCC\ModFormats
-copy %_NMMPATH%\bin\Release\ModFormats\FOMod.dll ..\..\output\NCC\ModFormats
-copy %_NMMPATH%\bin\Release\ModFormats\OMod.dll ..\..\output\NCC\ModFormats
+mkdir "%outputPath%\NCC\ModFormats"
+copy "%_NMMPATH%\bin\Release\ModFormats\FOMod.dll" "%outputPath%\NCC\ModFormats"
+copy "%_NMMPATH%\bin\Release\ModFormats\OMod.dll" "%outputPath%\NCC\ModFormats"
 
-mkdir ..\..\output\NCC\ScriptTypes
-copy %_NMMPATH%\bin\Release\ScriptTypes\Antlr*.dll ..\..\output\NCC\ScriptTypes
-copy %_NMMPATH%\bin\Release\ScriptTypes\CSharpScript.dll ..\..\output\NCC\ScriptTypes
-copy %_NMMPATH%\bin\Release\ScriptTypes\ModScript.dll ..\..\output\NCC\ScriptTypes
-copy %_NMMPATH%\bin\Release\ScriptTypes\XmlScript.dll ..\..\output\NCC\ScriptTypes
+mkdir  "%outputPath%\NCC\ScriptTypes"
+copy "%_NMMPATH%\bin\Release\ScriptTypes\Antlr*.dll" "%outputPath%\NCC\ScriptTypes"
+copy "%_NMMPATH%\bin\Release\ScriptTypes\CSharpScript.dll" "%outputPath%\NCC\ScriptTypes"
+copy "%_NMMPATH%\bin\Release\ScriptTypes\ModScript.dll" "%outputPath%\NCC\ScriptTypes"
+copy "%_NMMPATH%\bin\Release\ScriptTypes\XmlScript.dll" "%outputPath%\NCC\ScriptTypes"
 
-mkdir ..\..\output\NCC\data
-copy %_NMMPATH%\bin\Release\data\7z-32bit.dll ..\..\output\NCC\data
-copy %_NMMPATH%\bin\Release\data\7z-64bit.dll ..\..\output\NCC\data
+mkdir  "%outputPath%\NCC\data"
+copy "%_NMMPATH%\bin\Release\data\7z-32bit.dll" "%outputPath%\NCC\data"
+copy "%_NMMPATH%\bin\Release\data\7z-64bit.dll" "%outputPath%\NCC\data"
+
+rem copy the CLI binaries
+
+copy "%_NMMCLIPATH%bin\Release\NexusClientCLI.exe" "%outputPath%\NCC"
+copy "%_NMMCLIPATH%bin\Release\NexusClientCLI.exe.config" "%outputPath%\NCC"
+
+goto end
+
+:end
+exit 0
